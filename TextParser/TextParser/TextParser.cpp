f6054b31577f78cf4f166ac1631f2db384e040d3
Brozerker <chris.randall619@yahoo.com>
2015-12-09 13:37:17 -0800

    file being read

--- TextParser/TextParser/TextParser.cpp
+++ TextParser/TextParser/TextParser.cpp
@@ -2,12 +2,30 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <vector>
+#include <fstream>
 #include <iostream>
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "Hello World" << endl;
+	ifstream file;
+	file.open("text.txt");
+	vector<string> vector;
+	while (file) {
+		string temp;
+		file >> temp;
+		cout << temp << endl;
+		vector.push_back(temp);
+	}
+	cout << "vector is " << vector.size() << " words long";
 	return 0;
+}
+
+void toLowerCase(string * input) {
+	for (int i = 0; i < input->size(); ++i) {
+		input[i] = tolower(input->at(i));
+	}
 }
 
